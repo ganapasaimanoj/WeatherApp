5adc3f9fae5efbe51c1d6df832407650e657e10e
Manoj <ganapasaimanoj@gmail.com>
2019-09-26 19:51:25 +0530

    clear

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+        document.getElementById('lat').value = position.coords.latitude;
+        document.getElementById('lon').value = position.coords.longitude;
+    });
+})();
+
+document.getElementById('getBtn').addEventListener('click', getWeatherInfo);
+
+function getWeatherInfo(event) {
+    event.preventDefault();
+
+    const apiKey = 'Pk69Wv4dCv2y1evoN5sdgCjaJ0uqlCQc';
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+
+    const geoUrl = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat},${lon}`;
+    fetch(geoUrl).then(response => response.json()).then(data => {
+        collectWeatherInfo(data.Key);
+    }).catch(error => console.log(error));
+
+    function collectWeatherInfo(cityKey) {
+        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`;
+        fetch(weatherUrl).then(response => response.json()).then(data => {
+            displayWeather(data[0]);
+        }).catch(error => console.log(error));
+    }
+
+    function displayWeather(temperature) {
+        console.log(temperature);
+        document.getElementById('weatherText').textContent = temperature.WeatherText;
+        document.getElementById('weatherImage').setAttribute('src', `images/${temperature.WeatherIcon}.png`);
+        document.getElementById('weather').innerHTML = `${Math.round(temperature.Temperature.Metric.Value)}<sup>&#176;</sup>C`;
+    }
+}
